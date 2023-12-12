28304182cbf35ec7cecc7452869a7033eb088169
SiddharthEngineer <54515876+SiddharthEngineer@users.noreply.github.com>
2023-12-12 11:25:43 -0600

    Quick fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def main():
     df = create_or_load_csv()
 
     # User enters their name
-    user_name = st.text_input("Enter your name:", value="Siddharth")
+    user_name = st.text_input("Enter your name:", placeholder="Siddharth")
 
     # Select mode
     page = st.selectbox("What do you want to do?", ["Create My Wishlist", "Buy For Others"])
@@ -29,9 +29,9 @@ def main():
         st.header("Item Details")
 
         # Text input for item, description, and link
-        item = st.text_input("Item Name:", value="Socks")
-        description = st.text_input("Description:", value="8 is my shoe size")
-        link = st.text_input("Link:", value="amazon.com/cool-guy-socks")
+        item = st.text_input("Item Name:", placeholder="Sweater")
+        description = st.text_input("Description:", placeholder="Size L")
+        link = st.text_input("Link:", placeholder="amazon.com/ugly-xmas-sweater")
 
         # Submit button
         if st.button("Add Item"):
@@ -55,63 +55,67 @@ def main():
     
     elif page == "Buy For Others":
 
-        # Get a list of unique names in the DataFrame
-        others_names = df['Name'].unique().tolist()
-        others_names.remove(user_name)
+        try:
 
-        # Dropdown for selecting a person
-        selected_person = st.selectbox("Select a person to view their list:", others_names)
+            # Get a list of unique names in the DataFrame
+            others_names = df['Name'].unique().tolist()
+            others_names.remove(user_name)
 
-        # Display the selected person's list
-        st.header(f"{selected_person}'s Wishlist")
-        selected_person_wishlist = df[df['Name'] == selected_person]
-        selected_person_wishlist.insert(0, "Select", False)
-        edited_selection_df = st.data_editor(selected_person_wishlist[["Select", "Name", "Item", "Description", "Link", "Claimed By"]])
+            # Dropdown for selecting a person
+            selected_person = st.selectbox("Select a person to view their list:", others_names)
 
-        col1, col2, blank = st.columns([1, 2, 3])
-        if col1.button("Claim Items"):
-            selections = selected_person_wishlist[edited_selection_df["Select"]].index.to_list()
-            for selection in selections:
-                row = df.iloc[selection]
-                if row['Claimed By'] is None or pd.isna(row['Claimed By']):
-                    df.iloc[selection, df.columns.get_loc('Claimed By')] = user_name
-                    st.success(f"Successfully claimed {row['Item']} for {row['Name']}.")
-                elif row['Claimed By'] == user_name:
-                    st.write("You've already claimed this item.")
-                else:
-                    st.error(f"Someone else has already claimed {row['Item']} for {row['Name']}.")
-            df.to_csv('xmas_list.csv', index=False)
+            # Display the selected person's list
+            st.header(f"{selected_person}'s Wishlist")
+            selected_person_wishlist = df[df['Name'] == selected_person]
+            selected_person_wishlist.insert(0, "Select", False)
+            edited_selection_df = st.data_editor(selected_person_wishlist[["Select", "Name", "Item", "Description", "Link", "Claimed By"]])
 
-        if col2.button("Unclaim Items", key=1):
-            selections = selected_person_wishlist[edited_selection_df["Select"]].index.to_list()
-            for selection in selections:
-                row = df.iloc[selection]
-                if row['Claimed By'] == user_name:
-                    df.iloc[selection, df.columns.get_loc('Claimed By')] = None
-                    st.success(f"Successfully unclaimed {row['Item']} for {row['Name']}.")
-                else:
-                    st.error(f"You hadn't claimed buying {row['Item']} for {row['Name']}.")
-            df.to_csv('xmas_list.csv', index=False)
-        
-        # Complete list of all claimed items
-        st.header("My Shopping List")
+            col1, col2, blank = st.columns([1, 2, 3])
+            if col1.button("Claim Items"):
+                selections = selected_person_wishlist[edited_selection_df["Select"]].index.to_list()
+                for selection in selections:
+                    row = df.iloc[selection]
+                    if row['Claimed By'] is None or pd.isna(row['Claimed By']):
+                        df.iloc[selection, df.columns.get_loc('Claimed By')] = user_name
+                        st.success(f"Successfully claimed {row['Item']} for {row['Name']}.")
+                    elif row['Claimed By'] == user_name:
+                        st.write("You've already claimed this item.")
+                    else:
+                        st.error(f"Someone else has already claimed {row['Item']} for {row['Name']}.")
+                df.to_csv('xmas_list.csv', index=False)
 
-        # Display the selected person's list
-        claimed_items = df[df['Claimed By'] == user_name]
-        claimed_items.insert(0, "Select", False)
-        shopping_list = st.data_editor(claimed_items[["Select", "Name", "Item", "Description", "Link", "Claimed By"]])
+            if col2.button("Unclaim Items", key=1):
+                selections = selected_person_wishlist[edited_selection_df["Select"]].index.to_list()
+                for selection in selections:
+                    row = df.iloc[selection]
+                    if row['Claimed By'] == user_name:
+                        df.iloc[selection, df.columns.get_loc('Claimed By')] = None
+                        st.success(f"Successfully unclaimed {row['Item']} for {row['Name']}.")
+                    else:
+                        st.error(f"You hadn't claimed buying {row['Item']} for {row['Name']}.")
+                df.to_csv('xmas_list.csv', index=False)
+            
+            # Complete list of all claimed items
+            st.header("My Shopping List")
 
-        if st.button("Unclaim Items", key=2):
-            selections = claimed_items[shopping_list["Select"]].index.to_list()
-            for selection in selections:
-                row = df.iloc[selection]
-                if row['Claimed By'] == user_name:
-                    df.iloc[selection, df.columns.get_loc('Claimed By')] = None
-                    st.success(f"Successfully unclaimed {row['Item']} for {row['Name']}.")
-                else:
-                    st.error(f"You hadn't claimed buying {row['Item']} for {row['Name']}.")
-            df.to_csv('xmas_list.csv', index=False)
+            # Display the selected person's list
+            claimed_items = df[df['Claimed By'] == user_name]
+            claimed_items.insert(0, "Select", False)
+            shopping_list = st.data_editor(claimed_items[["Select", "Name", "Item", "Description", "Link", "Claimed By"]])
 
+            if st.button("Unclaim Items", key=2):
+                selections = claimed_items[shopping_list["Select"]].index.to_list()
+                for selection in selections:
+                    row = df.iloc[selection]
+                    if row['Claimed By'] == user_name:
+                        df.iloc[selection, df.columns.get_loc('Claimed By')] = None
+                        st.success(f"Successfully unclaimed {row['Item']} for {row['Name']}.")
+                    else:
+                        st.error(f"You hadn't claimed buying {row['Item']} for {row['Name']}.")
+                df.to_csv('xmas_list.csv', index=False)
+
+        except:
+            st.error("No one has entered a list. Tell people to use the app!")
 
 if __name__ == "__main__":
     main()
